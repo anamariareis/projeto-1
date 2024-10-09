880c9aa164a91e76b1d3abdfffcc5d99d518f976
anamariareis <142536903+anamariareis@users.noreply.github.com>
2024-10-09 08:46:41 -0300

    d

---
+++ perguntas.js
@@ -0,0 +1,119 @@
+criaCartão(
+    'Matematica',
+    'Como se calcula a some dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartão(
+    'História',
+    'Quanto tempo durou a Ditadura Civil-Militar no Brasil?',
+    '21 anos'
+)
+
+criaCartão(
+    'Português',
+    'O que é um verbo?',
+    'É uma classe gramatical que expressa ações, estados, fenômenos naturais, localizando-os no tempo'
+)
+
+criaCartão(
+    'Artes',
+    'que cor se obtem na mistura das cores azul e vermelho?',
+    'cor roxa ou violeta'
+)
+
+criaCartão(
+    'Biologia',
+    'Quantos ossos tem o corpo humano adulto? ',
+    '206 ossos'
+)
+
+criaCartão(
+    'Geografia',
+    'Quais são todos os continentes?',
+    'America, Europa, Africa, Asia, Antartica e Oceania'
+)
+
+criaCartão(
+    'Física',
+    'O que é MVU',
+    'Movimento Uniformemente Variado- é um movimento em que a velocidade varia de forma constante a cada segundo '
+)
+
+criaCartão(
+    'Química',
+    'O que são hidrocarbonetos',
+    'Compostos binários de Hidrogênio e Carbono'
+)
+
+criaCartão(
+    'Educação Financeira',
+    'Quanto de impostos o Brasil cobra por produtos importados',
+    'O Brasil cobra 20% para produtos abaixo de U$50, e acima 60%'
+)
+
+criaCartão(
+    'Empreendedorismo',
+    'O que é um perfil empreendedor?',
+    'É uma soma de atitudes e caracteristica que ajudam o profissional em sua trajetória'
+)
+
+criaCartão(
+    'Geografia',
+    'A Russia esta localizada na Ásia ou na Europa?',
+    ' Ambos os continentes'
+)
+
+criaCartão(
+    'História',
+    'Quais foram as principais causas da queda do Império Romano do Ocidente',
+    'Invasões barbáras, instabilidade política e econômica'
+)
+
+criaCartão(
+    'Filosofia',
+    'Qual o filosofo responsável pelo Mito da Caverna?',
+    'Platão'
+)
+
+criaCartão(
+    'Geografia',
+    'qual o menor país do mundo?',
+    'Vaticano'
+)
+
+criaCartão(
+    'Artes',
+    'Quem pintou a Monalisa',
+    'Leonardo da Vinci'
+)
+
+criaCartão(
+    'Matematica',
+    'Como se calcula a some dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartão(
+    'Matematica',
+    'Como se calcula a some dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartão(
+    'Matematica',
+    'Como se calcula a some dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartão(
+    'Matematica',
+    'Como se calcula a some dos termos de uma P.A',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartão(
+    '',
+    '',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
