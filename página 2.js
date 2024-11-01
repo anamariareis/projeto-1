9335c4c5f0f766a80abdf901b1698f657732e739
anamariareis <142536903+anamariareis@users.noreply.github.com>
2024-11-01 08:22:56 -0300

    a

--- página 2.js
+++ página 2.js
@@ -3,7 +3,7 @@ function criaCartão(categoria, pergunta, resposta){
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
     cartao.innerHTML = `
-    <div class="conteudo-cartao">
+    <div class="conteudo-cartao" onclick = "mostrarResposta(this)">
        <h3>${categoria}</h3>
        <div class="pergunta-cartao">
        <p>${pergunta}</p>
@@ -17,4 +17,13 @@ function criaCartão(categoria, pergunta, resposta){
    }
 
    function mostrarResposta(){
-    console resposta = document.querySelector('.resposta-cartao');
+    const resposta = document.querySelector('.resposta-cartao');
+
+    if(resposta.style.display === 'none'){
+resposta.style.display = 'block';
+    }
+    else{
+        resposta.style.display = 'none';
+    }
+
+   }
